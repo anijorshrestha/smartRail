8a0e645e6c995c61e2331542d0d34bea7a9794fc
acharyasulochana <acharyasulochana21@gmail.com>
2021-01-04 19:34:28 +0100

    url Updated

--- smartWindow/url.py
+++ smartWindow/url.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.smartwindow, name='main'),
     path('video_feed_1/', views.video_feed_1, name="video-feed-1"),
     path('geolocation/', views.geolocation, name="geolocation"),
+    path('index/',views.index, name="index"),
 
 ]
