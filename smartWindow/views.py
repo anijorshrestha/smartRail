854121837277d29f9bdd7bd5a473eb3370dcd4ce
acharyasulochana <acharyasulochana21@gmail.com>
2021-03-22 21:41:48 +0100

    this is test

--- smartWindow/views.py
+++ smartWindow/views.py
@@ -45,17 +45,6 @@ def stream_1():
 
     print(classes)
 
-    # construct the argument parse and parse the arguments
-    # ap = argparse.ArgumentParser()
-    # ap.add_argument("-i", "--input", required=True,	help="path to input video")
-    # ap.add_argument("-o", "--output", required=True,	help="path to output video")
-    # ap.add_argument("-y", "--yolo", required=True, help="base path to YOLO directory")
-    # ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
-    # ap.add_argument("-t", "--threshold", type=float, default=0.3, help="threshold when applyong non-maxima suppression")
-    # args = vars(ap.parse_args())
-    #
-    #
-    print("till here")
 
     # load the COCO class labels our YOLO model was trained on
     labelsPath = 'smartWindow/yolo/coco.names'
